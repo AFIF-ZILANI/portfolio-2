c61cef18d57daf062ac4a0f2f758149994f3bc06
AFIF ZILANI <afifzilani4566@gmail.com>
2025-06-25 17:40:17 +0600

    feat: update Home component to include Projects and Experience sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import About from "@/components/sections/About";
+import Experience from "@/components/sections/Experience";
 import Hero from "@/components/sections/Hero";
+import Projects from "@/components/sections/Projects";
 import Skills from "@/components/sections/Skills";
 
 export default function Home() {
     return (
-        <div className="space-y-4">
+        <div>
             <Hero />
             <About />
             <Skills />
+            <Projects />
+            <Experience />
         </div>
     );
 }
