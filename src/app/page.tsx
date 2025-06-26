fbfa39d4926d65d58ab6940e55da627c126fa31f
AFIF ZILANI <afifzilani4566@gmail.com>
2025-06-26 20:28:57 +0600

    feat: add contact section with SplitText animation and ContactForm component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import About from "@/components/sections/About";
+import ContactForm from "@/components/sections/ContactMe";
 import Experience from "@/components/sections/Experience";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
 import Skills from "@/components/sections/Skills";
+import SplitText from "@/components/blocks/TextAnimations/SplitText/SplitText";
 
 export default function Home() {
     return (
@@ -12,6 +14,22 @@ export default function Home() {
             <Skills />
             <Projects />
             <Experience />
+            <section id="contact" className="flex flex-col sm:w-xl items-center mb-32 mx-auto">
+                <SplitText
+                    text="Contact Me"
+                    className="text-4xl font-semibold text-center text-primary mb-12"
+                    delay={100}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                />
+                <ContactForm />
+            </section>
         </div>
     );
 }
