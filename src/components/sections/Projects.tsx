ae2ee35bca2870dbae63ecd0ad2a2bdb41a1ec45
AFIF ZILANI <afifzilani4566@gmail.com>
2025-06-26 20:27:36 +0600

    feat: adjust Projects section layout for improved spacing and responsiveness

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -60,12 +60,12 @@ const projectList = [
 
 export default function Projects() {
     return (
-        <section id="projects" className="min-h-screen mb-48">
+        <section id="projects" className="min-h-screen mb-12">
             <div className="relative w-[90vw] mx-auto mb-4">
                 <TextPressure
                     text="Projects"
                     flex={true}
-                    alpha={true}
+                    alpha={false}
                     stroke={false}
                     width={true}
                     weight={true}
@@ -75,12 +75,13 @@ export default function Projects() {
                     minFontSize={36}
                 />
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-12 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 md:px-12 ">
                 {projectList.map(({ name, description, github_url, image, live_url }) => (
                     <div key={name} className="rounded-2xl shadow p-6 bg-card">
                         <Image
                             src={image}
                             alt={`${name} screenshot`}
+                            loading="lazy"
                             className="mb-4 w-full h-40 object-cover rounded-lg"
                         />
                         <h3 className="text-xl font-semibold text-primary mb-2">{name}</h3>
